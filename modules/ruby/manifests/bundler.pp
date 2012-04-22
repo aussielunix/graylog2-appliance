6cb30ae1ed790f1c314c47170d3377acbb4f60c8
Mick Pollard <aussielunix@gmail.com>
2012-04-22 17:40:29 +1000

    add rubygems bin/ to $PATH

--- modules/ruby/manifests/bundler.pp
+++ modules/ruby/manifests/bundler.pp
@@ -4,4 +4,12 @@ class ruby::bundler {
     provider => 'gem',
     require  => Class['ruby::install']
   }
+
+  file {'/etc/profile.d/rubygems.sh':
+    source  => 'puppet:///modules/ruby/etc/profile.d/rubygems.sh',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    require => Class['ruby::install']
+  }
 }
