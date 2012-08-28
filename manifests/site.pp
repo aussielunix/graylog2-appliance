c3fc6e3cb4c713dea7fdc06a1b0e0af17fcee0a2
Mick Pollard <aussielunix@gmail.com>
2012-08-29 07:53:49 +1000

    correctly reference modules/classes in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
-  include aussielunix         #setup aussielunix apt repo
+  include aussielunix::repo   #setup aussielunix apt repo
   include elasticsearch
   include graylog2            # meta class installs web and server on same host
-  include mongodb
+  include mongo
 }
