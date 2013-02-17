d979f603099c0469ea722ad5c5c1e2b2c2d6cd13
Mick Pollard <aussielunix@gmail.com>
2013-02-17 21:22:06 +1100

    complete refactor to make use of new module design patterns

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,17 @@
 node default {
-  include aussielunix::repo   #setup aussielunix apt repo
-  include graylog2            # meta class installs web and server on same host
   include mongo
-  include elasticsearch
 
-  class { 'elasticsearch::config':
-    user     => 'elasticsearch',
-    group    => 'elasticsearch',
-    minmem   => '128m',
-    maxmem   => '512m',
-    pathlogs => '/var/log/elasticsearch',
-    pathconf => '/etc/elasticsearch',
-    pathdata => '/var/lib/elasticsearch',
+  Apt::Source <| |> -> Package <| |>
+
+  class { 'aussielunix::repo': }
+  class { 'elasticsearch':
+    clustername => 'graylog2'
+  }
+  class { 'graylog2server':
+    es_cluster_name => 'graylog2'
+  }
+  class { 'graylog2web':
+    enable => true,
+    start  => true
   }
 }
