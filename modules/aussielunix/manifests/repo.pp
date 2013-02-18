a64e45e3353107721d3a3b3b73fe0adac15d06de
Mick Pollard <aussielunix@gmail.com>
2013-02-18 11:22:34 +1100

    add parametre to install or remove an apt repo

--- modules/aussielunix/manifests/repo.pp
+++ modules/aussielunix/manifests/repo.pp
@@ -1,11 +1,16 @@
 # aussielunix::repo
 #
-class aussielunix::repo {
+class aussielunix::repo
+(
+  $ensure = 'present'
+)
+{
   # ensure that apt::source is available to us
   include apt
 
   # make sure the aussielunix apt repos is available
   apt::source {'aussielunix':
+    ensure      => $ensure,
     location    => 'http://ppa.lunix.com.au/ubuntu',
     release     => 'precise',
     repos       => 'main',
